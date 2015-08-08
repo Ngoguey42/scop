85300723140a2dbf850055afbb3e0614afdca254
Nicolas GOGUEY <ngoguey@e1r7p22.42.fr>
2015-08-08 12:53:36 +0200

    implemented srcs/configuration/mesh_fill*.c

---
+++ srcs/configuration/mesh_fill2.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mesh_fill2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngoguey <ngoguey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/08/08 12:52:39 by ngoguey           #+#    #+#             */
+/*   Updated: 2015/08/08 12:52:40 by ngoguey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "scop.h"
+#include "objmodel.h"
+#include <math.h>
+#include <string.h>
+
+/*
+**  [[[cog
+**  import cog
+**  cog.outl("*" "/")
+**  from scop_confloader import output_meshfill
+**  output_meshfill(5)
+**  cog.out("/" "*")
+**  ]]]
+*/
+
+/*
+**  [[[end]]]
+*/
